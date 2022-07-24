bf71e059296d54b33fdeb75c9ab46c671d4f202f
Poria Cattus <poria@daosquare.io>
2022-07-25 01:26:15 +0800

    ✨ call pod container!

--- packages/node/route.js
+++ packages/node/route.js
@@ -13,6 +13,7 @@ const snapshot = {};
 export const getRoutesAndIpfs = async () => {
   const client = await ipfsClient();
 
+  // QmXv15dEXvRuwe9WNmmS1gruTEMm8TAEwZ61XFnPVjz96T
   const create = async (req, res) => {
     try {
       const cid = req.body.cid;
@@ -50,24 +51,140 @@ export const getRoutesAndIpfs = async () => {
         }
       }
 
-      console.log(pods);
-      // memory,exports,bindHelper
-      const bindHelper = pods[cid].containerRuntimes["Hello"].bindHelper;
-      const helloString = bindHelper.lowerString("just hello!");
-      console.log({ helloString, x: bindHelper.lowerString });
-      pods[cid].containerRuntimes["Hello"].exports.storeWhat(helloString);
-      return pods;
+      return `pod ${cid} deployed success!`;
       // return "success";
     } catch (err) {
       console.log(err);
       throw boom.boomify(err);
     }
   };
+
+  const call = async (req, res) => {
+    let { podId, container, method, params } = req.body;
+    if (!podId || !container || !method) {
+      throw boom.boomify(
+        new Error("podId or container or method may be empty")
+      );
+    }
+
+    if (!params) {
+      params = [];
+    }
+
+    const pod = pods[podId];
+
+    if (!pod) {
+      throw boom.boomify(new Error("can't find pod in this node"));
+    }
+
+    const config = pod.config;
+
+    console.log(config);
+
+    const findContainer = config.pod.filter(c => {
+      return c.container === container
+    })
+
+    if (findContainer.length === 0) {
+      throw boom.boomify(
+        new Error(`pod ${podId} have no container: ${container}`)
+      );
+    }
+
+    console.log(findContainer[0]);
+
+    const handlers = findContainer[0].handlers;
+
+    console.log({handlers})
+
+    const findMethod = handlers.filter((handler) => {
+      console.log({handler})
+      return handler.handler.method === method;
+    });
+
+    if (findMethod.length === 0) {
+      throw boom.boomify(new Error(`pod ${podId} have no method: ${method}`));
+    }
+
+    const handler = findMethod[0];
+    const paramsConfig = handler.handler.params;
+    console.log(paramsConfig);
+
+    const input = [];
+
+    if (paramsConfig) {
+      if (paramsConfig.length !== params.length) {
+        throw boom.boomify(
+          new Error(
+            `params count is wrong, expect count is ${paramsConfig.length} but get ${params.length}`
+          )
+        );
+      }
+      // console.log(pods[podId])
+      // console.log(pods[podId].containerRuntimes)
+      // console.log(pods[podId].containerRuntimes[container].bindHelper)
+      // return "www"
+      const bindHelper = pods[podId].containerRuntimes[container].bindHelper;
+      paramsConfig.forEach((param, index) => {
+        const paramType = typeof params[index];
+        if (param.type === "string") {
+          if (paramType !== "string") {
+            throw boom.boomify(
+              new Error(
+                `param ${param.name} should be string, but give ${paramType}`
+              )
+            );
+          }
+          input.push(bindHelper.lowerString(params[index]));
+        } else if (param.type === "i32") {
+          if (paramType !== "number" || !Number.isInteger(params[index])) {
+            throw boom.boomify(
+              new Error(
+                `param ${param.name} should be i32, but give ${paramType}`
+              )
+            );
+          }
+          input.push(params[index]);
+        }
+      });
+    }
+
+    // console.log({input})
+
+    // console.log({ podId, container, method, params });
+
+    pods[podId].containerRuntimes[container].exports[method](...input);
+
+
+    // snapshot {
+    //   "QmXv15dEXvRuwe9WNmmS1gruTEMm8TAEwZ61XFnPVjz96T": [
+    //     {
+    //       "entity": "Test",
+    //       "id": "id_12",
+    //       "data": null
+    //     },
+    //     {
+    //       "entity": "Test",
+    //       "id": "id_12",
+    //       "data": {
+    //         "say": "just hello!"
+    //       }
+    //     }
+    //   ]
+    // }
+
+    return "success";
+  };
   const routes = [
     {
       method: "POST",
       url: "/create",
       handler: create,
+    },
+    {
+      method: "POST",
+      url: "/call",
+      handler: call,
     },
   ];
   return { routes, ipfsClient: client };
